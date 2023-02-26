b9dcd6771e42644b73866ce6879c5bcb9caaabab
muratbaseren <muratbaseren@gmail.com>
2023-02-27 01:29:48 +0300

    notebook app page sample

---
+++ BlazorServerApp1/Pages/Notebooks/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServerApp1.Pages.Notebooks
+{
+    public class Note
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Text { get; set; } = "";
+        public string Color { get; set; } = "";
+        public bool IsEditing { get; set; } = false;
+        public bool IsDeleting { get; set; } = false;
+    }
+}
